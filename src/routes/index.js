f9353c88ba87d537903eaa59bbff089ef4639a12
houriasayah <144814481+houriasayah@users.noreply.github.com>
2024-02-07 09:19:55 +0100

    Update index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,21 @@
 import { Router } from 'express';
+import axios from 'axios';
 
 const router = Router();
 
-router.get('/', (req, res) => {
-  req.log.info('/ request recieved');
-
-  res.send('Hello World!');
+router.get('/country/:name', async (req, res) => {
+    const countryName = req.params.name;
+    try {
+        // Effectuer une requête à l'API REST Countries
+        const response = await axios.get(`https://restcountries.com/v3.1/name/${countryName}`);
+        // Extraire les données du premier pays trouvé
+        const countryData = response.data[0];
+        // Renvoyer les données du pays en réponse
+        res.json(countryData);
+    } catch (error) {
+        // En cas d'erreur, renvoyer une erreur 500 avec un message d'erreur
+        res.status(500).json({ message: 'Erreur lors de la récupération des informations sur le pays' });
+    }
 });
 
 export default router;
