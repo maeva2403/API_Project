2ae26034b3a0e82cdacb7dce20a7b44549f5d4cd
houriasayah <144814481+houriasayah@users.noreply.github.com>
2024-03-19 20:10:08 +0000

    modif : n'afficher qu'un project à la fois

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,18 +1,20 @@
+// Fonction pour récupérer les informations sur le pays en fonction du nom du pays
+const fetchCountryInfo = async (countryName) => {
+    const response = await fetch(`/api/country?name=${countryName}`);
+    return response.json();
+};
+
+// Fonction pour récupérer les informations sur les devises en fonction de la devise de pays
+const fetchCurrencyInfo = async (currency) => {
+    const response = await fetch(`/api/convert?base=${currency}`);
+    return response.json();
+};
+
 document.addEventListener('DOMContentLoaded', async () => {
-
     const countryInput = document.getElementById('country-input');
-
-    // Fonction pour récupérer les informations sur le pays en fonction du nom du pays
-    const fetchCountryInfo = async (countryName) => {
-        const response = await fetch(`/api/country?name=${countryName}`);
-        return response.json();
-    };
-
-    // Fonction pour récupérer les informations sur les devises en fonction de la devise de pays
-    const fetchCurrencyInfo = async (currency) => {
-        const response = await fetch(`/api/convert?base=${currency}`);
-        return response.json();
-    };
+    const projectsListElement = document.getElementById('projects-list');
+    const projectPanelElement = document.getElementById('project-panel');
+    const submitButton = document.getElementById('submit-btn');
 
     // Mise à jour de la liste des projets
     const updateProjectsList = async () => {
@@ -23,39 +25,27 @@ document.addEventListener('DOMContentLoaded', async () => {
 
         const currencyInfo = await fetchCurrencyInfo(currency);
 
-        const project = {
-            title: 'Country Info',
-            id: 1, // Vous devez définir un ID pour chaque projet
-            common_name: countryInfo.common_name,
-            official_name: countryInfo.official_name,
-            language: countryInfo.language,
-            region: countryInfo.region,
-            capital: countryInfo.capital,
-            currency: countryInfo.currency,
-            base: currencyInfo.base,
-            rates: currencyInfo.rates
-        };
+        projectPanelElement.replaceChildren(); // Remplace les enfants du panneau des projets
 
         const projectTitle = document.createElement('h2');
-        projectTitle.textContent = project.title;
+        projectTitle.textContent = 'Country Info';
         const projectDescription = document.createElement('p');
-        // projectDescription.classList.add('description')
         projectDescription.innerHTML = `
-            <strong>Common Name:</strong> ${project.common_name}<br>
-            <strong>Official Name:</strong> ${project.official_name}<br>
-            <strong>Language:</strong> ${project.language}<br>
-            <strong>Region:</strong> ${project.region}<br>
-            <strong>Capital:</strong> ${project.capital}<br>
-            <strong>Currency:</strong> ${project.currency}<br>
-            <strong>Base Currency:</strong> ${project.base}<br>
-            <strong>Rates:</strong> ${JSON.stringify(project.rates)}<br>
+            <strong>Common Name:</strong> ${countryInfo.common_name}<br>
+            <strong>Official Name:</strong> ${countryInfo.official_name}<br>
+            <strong>Language:</strong> ${countryInfo.language}<br>
+            <strong>Region:</strong> ${countryInfo.region}<br>
+            <strong>Capital:</strong> ${countryInfo.capital}<br>
+            <strong>Currency:</strong> ${countryInfo.currency}<br>
+            <strong>Base Currency:</strong> ${currencyInfo.base}<br>
+            <strong>Rates:</strong> ${JSON.stringify(currencyInfo.rates)}<br>
         `;
-        document.body.appendChild(projectTitle);
-        document.body.appendChild(projectDescription);
+        projectPanelElement.appendChild(projectTitle);
+        projectPanelElement.appendChild(projectDescription);
     };
 
     // Écouteur d'événement pour le clic sur le bouton d'envoi
-    document.getElementById('submit-btn').addEventListener('click', async () => {
+    submitButton.addEventListener('click', async () => {
         await updateProjectsList();
     });
 });
