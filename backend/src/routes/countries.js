7a21630ae82d251e3ba0e0f84f8a43c64990bb1a
houriasayah <144814481+houriasayah@users.noreply.github.com>
2024-03-20 19:28:40 +0000

    comments

--- backend/src/routes/countries.js
+++ backend/src/routes/countries.js
@@ -1,67 +1,88 @@
+// Calling the REST Countries API to retrieve information about countries
+
 import { Router } from 'express';
+
 
 const router = Router();
 
+// unction to fetch data of all countries
 const getAllCountries = async () => {
+  // URL for fetching country data
   const url = `https://restcountries.com/v3.1/all`;
-    const options = {
-      method : 'GET',
-      headers: {
-        accept : 'application/json',
+
+  // Options for the fetch request
+  const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
     },
   };
 
+  // Fetching data from the API
   const response = await fetch(url, options);
+
+  // Parsing the JSON response
   const json = await response.json();
 
+  // Returning the JSON data
   return json;
 }
 
-// Fonction pour rechercher un pays par son nom
+// Function to find a country by its name
 const findCountryByName = (name) => (country) => {
+  // Comparing the common name of the country with the provided name (case insensitive)
   return country.name.common.toLowerCase() === name.toLowerCase();
 };
 
+// Route handler for GET requests to '/country'
 router.get('/country', async (req, res) => {
+  // Extracting the country name from the query parameters
   const countryName = req.query.name;
+
+  // Fetching data of all countries
   const countries = await getAllCountries();
 
+  // Handling the case when no country name is provided in the query parameters
   if (!countryName) {
-    // Si aucun nom de pays n'est fourni, renvoyer les détails du premier pays
+    // Retrieving details of the first country
     const firstCountry = countries[0];
+    
+    // Sending JSON response with details of the first country
     return res.json({
       common_name: firstCountry.name.common,
       official_name: firstCountry.name.official,
       language: Object.values(firstCountry.languages),
       region: firstCountry.region,
-      capital : firstCountry.capital,
+      capital: firstCountry.capital,
       currency: Object.keys(firstCountry.currencies)[0],
       latlng: firstCountry.latlng
-      // Assurez-vous d'ajouter d'autres propriétés du premier pays ici
+      // You can add more properties of the first country here
     });
   };
 
-
+  // Finding the country object by name
   const country = countries.find(findCountryByName(countryName));
 
+  // Handling the case when the country is not found
   if (!country) {
-    // Si le pays n'est pas trouvé, retourner une réponse 404
+    // Sending a 404 response with a message indicating that the country was not found
     return res.status(404).json({
-      status: 404, 
+      status: 404,
       message: `Country '${countryName}' not found`
     });
   }
 
-  // Renvoyer les informations du pays en réponse
+  // Sending JSON response with details of the found country
   res.json({
-    common_name : country.name.common,
+    common_name: country.name.common,
     official_name: country.name.official,
     language: Object.values(country.languages),
     region: country.region,
-    capital : country.capital,
+    capital: country.capital,
     currency: Object.keys(country.currencies)[0],
     latlng: country.latlng
   });
 });
 
+// Exporting the router instance
 export default router;
