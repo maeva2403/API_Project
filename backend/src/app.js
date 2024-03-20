9009c2de0d86c6351bb849a4edbf46913b3ac374
houriasayah <144814481+houriasayah@users.noreply.github.com>
2024-03-20 20:17:23 +0000

    le début du swagger

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,18 +2,25 @@ import express from 'express';
 import logger from 'pino-http';
 import countriesRoutes from './routes/countries.js';
 import deviseRoutes from './routes/devise.js';
+import swaggerUi from 'swagger-ui-express';
+import YAML from 'yamljs';
 
 const app = express();
+
+// Charger la documentation Swagger à partir du fichier YAML ou JSON
+const swaggerDocument = YAML.load('../docs/swagger.yaml');
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(logger({ level: process.env.NODE_ENV === 'test' ? 'error' : 'info' }));
 
-// first test without api
+// Route d'accueil
 app.get('/', (req, res) => {
-    res.send('Welcome to the homepage'); // Ou tout autre contenu que vous souhaitez renvoyer pour la page d'accueil
+    res.send('Welcome to the homepage');
 });
   
+// Routes des pays et des devises
 app.use('/api', countriesRoutes);
 app.use('/api', deviseRoutes);
 
