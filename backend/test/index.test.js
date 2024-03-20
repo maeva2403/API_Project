563efd7ad1b67777ba843d5751cf5c671fe9b125
houriasayah <144814481+houriasayah@users.noreply.github.com>
2024-03-20 15:10:04 +0000

    sans test

--- backend/test/index.test.js
+++ backend/test/index.test.js
@@ -9,36 +9,3 @@ describe('GET /', () => {
     assert.strictEqual(response.status, 200);
   });
 });
-
-
-describe('GET /api/country', () => {
-  it('should return details of a country with 200 status code if valid country name provided', async () => {
-    const countryName = 'France'; // Modifier avec un pays existant dans votre source de données
-
-    const response = await request(app).get(`/api/country?name=${countryName}`);
-
-    assert.strictEqual(response.status, 200);
-    assert.property(response.body, 'common_name');
-    assert.property(response.body, 'official_name');
-    // Assurez-vous d'ajouter d'autres assertions pour vérifier les autres propriétés
-  });
-
-  it('should return details of the first country with 200 status code if no country name provided', async () => {
-    const response = await request(app).get(`/api/country`);
-
-    assert.strictEqual(response.status, 200);
-    assert.property(response.body, 'common_name');
-    assert.property(response.body, 'official_name');
-    // Assurez-vous d'ajouter d'autres assertions pour vérifier les autres propriétés
-  });
-
-  it('should return 404 status code if invalid country name provided', async () => {
-    const invalidCountryName = 'InvalidCountryName'; // Un nom de pays qui n'existe pas dans votre source de données
-
-    const response = await request(app).get(`/api/country?name=${invalidCountryName}`);
-
-    assert.strictEqual(response.status, 404);
-    assert.strictEqual(response.body.status, 404);
-    assert.strictEqual(response.body.message, `We could not found a country with the name: ${invalidCountryName}`);
-  });
-});
