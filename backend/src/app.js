a1b0cfd4d15ae8a7a361db431c6004996793969a
houriasayah <144814481+houriasayah@users.noreply.github.com>
2024-03-22 12:57:42 +0000

    remise à zero du swagger

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,14 +2,9 @@ import express from 'express';
 import logger from 'pino-http'; 
 import countriesRoutes from './routes/countries.js'; 
 import deviseRoutes from './routes/devise.js'; 
-import swaggerUi from 'swagger-ui-express';
-import YAML from 'yamljs';
 
 const app = express();
 
-// Loading Swagger documentation from YAML file
-const swaggerDocument = YAML.load('../docs/swagger.yaml');
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument)); 
 
 app.use(express.json()); 
 app.use(express.urlencoded({ extended: false })); 
